9005900ba7ce9ced531f5707dbdde7896f18e3fb
Yorichi123qwx <164789522+Yorichi123qwx@users.noreply.github.com>
2024-08-06 14:42:56 +0300

    Update .بوت.js

--- plugins/.بوت.js
+++ plugins/.بوت.js
@@ -2,15 +2,15 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `*مــرحــبًــا〘 ${taguser} 〙 انـا سوكونا*\n *• هــل تـحـتـاج الـى مـسـاعـدة؟ ، اكـتـب﹙.اوامــر ╎ .قــائـمــة﹚*\n\n\n> *🚫╎مـمـنـوع اسـتـخـدام الـبـوت فـي الـدردشـات الـخـاصـة!*`;
+    let message = `*مــرحــبًــا〘 ${taguser} 〙 انـا يوريتشي*\n *• هــل تـحـتـاج الـى مـسـاعـدة؟ ، اكـتـب﹙.اوامــر ╎♡╎ .قــائـمــة﹚*\n\n\n> *🚫╎من فضلك يمنع استخدام البوت في الخاص!*`;
     await conn.sendMessage(m.chat, {
    react: {
- text: "🗿",
+ text: "😈",
  key: m.key,
    }
   })
 
-    conn.sendFile(m.chat, 'https://telegra.ph/file/d54800d1c431f3cc6e06b.jpg', 'video.mp4' , message, m);
+    conn.sendFile(m.chat, 'https://telegra.ph/file/c3b319c4e9fa528251b92.jpg', 'video.mp4' , message, m);
 };
 handler.help = ['bot'];
 handler.tag = ['dado'];

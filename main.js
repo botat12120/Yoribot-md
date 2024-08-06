1625ad139592184b8d8e2d4c3ece6bf9f098e06e
Yorichi123qwx <164789522+Yorichi123qwx@users.noreply.github.com>
2024-08-06 12:08:18 +0300

    Update main.js

--- main.js
+++ main.js
@@ -584,7 +584,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `[ 💀 ] وقت تشغيل بوت سوكونا:: ${uptime}`;
+  const bio = `[ 😈 ] وقت تشغيل بوت يوريتشي:: ${uptime}`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
